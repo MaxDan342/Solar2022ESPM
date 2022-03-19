b102d550729b3f117135dd5b91e933909ec07659
MaxDan342 <dgoulart56@gmail.com>
2022-03-18 22:40:48 -0300

    Rotação da terra

---
+++ Assets/Code/Rotacao.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacao : MonoBehaviour
+{
+    public float rot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("olá mundo Daniel");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, rot, 0);
+    }
+}
